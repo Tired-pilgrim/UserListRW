db8c3974142c728cef546fb919805bfd9e647a9b
Tired-pilgrim <biz@gmail.com>
2023-04-24 15:18:22 +0300

    Эксперимент с MarkupExtension

--- View/Windows/ViewHelper.cs
+++ View/Windows/ViewHelper.cs
@@ -1,26 +1,31 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
 using ViewModel;
 using ViewModelLib.Commands;
 using static Views.Windows.OpenSaveViewHelper;
 namespace Views.Windows
 {
-    public static class ViewHelper
+    [MarkupExtensionReturnType(typeof(RelayCommand))]
+    public class ViewHelper: MarkupExtension
     {
-        public static RelayCommand<DependencyObject> AddUserDialog { get; } = new RelayCommand<DependencyObject>(dobj =>
-        {
-            Window? currWin = Window.GetWindow(dobj);
-            if (currWin.DataContext != null)
+        public ViewHelper() 
+        { 
+            this.AddUserDialog = AddUserDialog;
+        }
+        private RelayCommand AddUserDialog { get; } = new RelayCommand( _ =>
             {
-                AddUzer _addUzer = new AddUzer()
+                if (currWin?.DataContext != null)
                 {
-                    DataContext = ((MainViewModel)currWin.DataContext).AddUserVM,
-                    Owner = currWin,
-                    WindowStartupLocation = WindowStartupLocation.CenterOwner
-                };
-                _addUzer.ShowDialog();
-            }
-        });
+                    AddUzer _addUzer = new AddUzer()
+                    {
+                        DataContext = ((MainViewModel)currWin.DataContext).AddUserVM,
+                        Owner = currWin,
+                        WindowStartupLocation = WindowStartupLocation.CenterOwner
+                    };
+                    _addUzer.ShowDialog();
+                }
+            });
         public static RelayCommand<ISaveOpen> OpenListUserDialog { get; } = new RelayCommand<ISaveOpen>(Ivm =>
         {
             _ = Ivm.OpenListUserAsync(OpenDial());
@@ -29,7 +34,14 @@ namespace Views.Windows
         {
             _ = Ivm.SaveListUser(SaveDial());
         });
-        
+        private static Window? currWin;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            IProvideValueTarget service = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget))!;
+            currWin = Window.GetWindow(service.TargetObject as DependencyObject);
+            return AddUserDialog;
+            
+        }
     }
  }
 
