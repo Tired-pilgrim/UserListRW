a07a7e80cab66c388a2a4a1cc31006ab55c33957
Tired-pilgrim <biz@gmail.com>
2023-04-30 15:43:17 +0300

    Удалил лишнее

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using Model;
-using System;
 using System.Collections;
 using System.Windows;
 using System.Windows.Data;
-using VievModelLib;
 using ViewModel;
-using Views;
 using Views.Windows;
 
 namespace View
@@ -16,24 +13,18 @@ namespace View
     public partial class App : Application
     {
         private readonly ValueProxy<ICollection> UsersSync = new ValueProxy<ICollection>();
-        //private readonly DialogsService _dialogsService;
-        private MessageHelper _messageHelper;
         public App()
         {
             MainModel _mainModel = new MainModel();
-            //_dialogsService = new DialogsService();
             UsersSync.ValueChanged += OnUsersChanged;
             MainViewModel vm = new(_mainModel);
             UsersSync.SetValueBinding(new Binding(nameof(MainViewModel.Users)) { Source = vm });
             MainWindow _mainWindow = new MainWindow() { DataContext = vm };
-            //_messageHelper = new MessageHelper(_mainWindow);
             _mainWindow.Show();
         }
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            //_dialogsService.Register(_messageHelper.MessageShow);
-            //_dialogsService.Register(_messageHelper.ShowErrorDialog);
             MessageDO.Register();
         }
 
