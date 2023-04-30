d8c8f9e1205bb5e6bf44fbe894562a57f15a32b3
Tired-pilgrim <biz@gmail.com>
2023-04-30 14:01:14 +0300

    Добавден  DependencyObject

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -16,26 +16,27 @@ namespace View
     public partial class App : Application
     {
         private readonly ValueProxy<ICollection> UsersSync = new ValueProxy<ICollection>();
-        private readonly DialogsService _dialogsService;
+        //private readonly DialogsService _dialogsService;
         private MessageHelper _messageHelper;
         public App()
         {
             MainModel _mainModel = new MainModel();
-            _dialogsService = new DialogsService();
+            //_dialogsService = new DialogsService();
             UsersSync.ValueChanged += OnUsersChanged;
-            MainViewModel vm = new(_mainModel, _dialogsService);
+            MainViewModel vm = new(_mainModel, DialogsService.Default);
             UsersSync.SetValueBinding(new Binding(nameof(MainViewModel.Users)) { Source = vm });
             MainWindow _mainWindow = new MainWindow() { DataContext = vm };
-            _messageHelper = new MessageHelper(_mainWindow);
+            //_messageHelper = new MessageHelper(_mainWindow);
             _mainWindow.Show();
         }
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            _dialogsService.Register(_messageHelper.MessageShow);
-            _dialogsService.Register(_messageHelper.ShowErrorDialog);
+            //_dialogsService.Register(_messageHelper.MessageShow);
+            //_dialogsService.Register(_messageHelper.ShowErrorDialog);
+            MessageDO.Register();
         }
-        
+
         private void OnUsersChanged(object? sender, DependencyPropertyChangedEventArgs e)
         {
             if (e.OldValue is ICollection old)
---
+++ View/Windows/MessageDO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using VievModelLib;
+
+namespace Views.Windows
+{
+    public class MessageDO : DependencyObject
+    {
+        private MessageDO() { }
+        public static readonly MessageDO Instance = new MessageDO();
+
+        public double InfoOpacity
+        {
+            get { return (double)GetValue(InfoOpacityProperty); }
+            set { SetValue(InfoOpacityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Opacity.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InfoOpacityProperty =
+            DependencyProperty.Register(nameof(InfoOpacity), typeof(double), typeof(MessageDO), new PropertyMetadata(0.0));
+
+        public string InfoText
+        {
+            get { return (string)GetValue(InfoTextProperty); }
+            set { SetValue(InfoTextProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for InfoText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InfoTextProperty =
+            DependencyProperty.Register(nameof(InfoText), typeof(string), typeof(MessageDO), new PropertyMetadata(string.Empty)
+            {
+                CoerceValueCallback = (d, e) => e ?? string.Empty
+            });
+
+        public static void Register() => Register(DialogsService.Default);
+
+        public static void Register(DialogsService service)
+        {
+            service.Register(new Action<Error>(ShowErrorDialog));
+            service.Register(new Action<Info>(MessageShow));
+        }
+        private static void ShowErrorDialog(Error message)
+        {
+            MessageBox.Show(message.error, "Список служащих");
+        }
+        private static readonly Storyboard ShowAnimation;
+        static MessageDO()
+        {
+            DoubleAnimationUsingKeyFrames doubleAnimation = new DoubleAnimationUsingKeyFrames();
+            doubleAnimation.KeyFrames.Add(new DiscreteDoubleKeyFrame(0, KeyTime.FromPercent(0)));
+            doubleAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(1, KeyTime.FromPercent(0.07)));
+            doubleAnimation.KeyFrames.Add(new DiscreteDoubleKeyFrame(1, KeyTime.FromPercent(0.8)));
+            doubleAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromPercent(1)));
+            doubleAnimation.Duration = TimeSpan.FromSeconds(5);
+
+            Storyboard.SetTarget(doubleAnimation, Instance);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(InfoOpacityProperty));
+
+            ShowAnimation = new Storyboard();
+            ShowAnimation.Children.Add(doubleAnimation);
+        }
+
+        private static void MessageShow(Info message)
+        {
+            _ = Instance.Dispatcher.BeginInvoke(() =>
+            {
+                Instance.InfoText = message.info;
+                ShowAnimation.Begin();
+            });
+        }
+    }
+}
